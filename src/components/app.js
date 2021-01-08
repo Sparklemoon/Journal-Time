3e1dab43a4d6e7ffed976e3c338c49a736a0856b
Markus George <gmarkus993@gmail.com>
2021-01-08 16:22:24 -0600

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import ViewPage from '../components/view-journal-entry'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+       <ViewPage/>
       </div>
     );
   }
